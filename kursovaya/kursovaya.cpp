6877ed43d2d319bf9972c68710ce4f28fa1f01b8
polinazharova <1132226165@pfur.ru>
2023-11-12 23:41:05 +0300

    the first task is done

---
+++ kursovaya/kursovaya.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <fstream>
+
+int main()
+{
+	std::vector <double> Hc;
+
+	int eps = 100;
+	for (int i{ 0 }; i <= 60000; i += eps) {
+		Hc.push_back(i);
+	}
+
+	double R0 = 6371;
+	double grav = 398600.4;
+	double Vc;
+
+	
+	std::ofstream fout;
+
+	try {
+		fout.open("Hc_Vc.txt");
+		for (auto& HIGH : Hc) {
+			Vc = sqrt((grav / (R0 + HIGH)));
+			fout << HIGH << "\t" << Vc << std::endl;
+		}
+		std::cout << "Data was succesfully written.";
+		fout.close();
+		return 0;
+	}
+	
+	catch (std::exception& ex) {
+		std::cout << "File opening error!";
+		fout.close();
+		return 0;
+	}
+
+}
+
