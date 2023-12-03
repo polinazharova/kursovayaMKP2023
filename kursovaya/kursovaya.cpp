b21c618e973a7dd6b0f5f7b3d76968dc7c699c00
polinazharova <1132226165@pfur.ru>
2023-12-03 20:26:35 +0300

    the second task is done

--- kursovaya/kursovaya.cpp
+++ kursovaya/kursovaya.cpp
@@ -1,41 +1,43 @@
-﻿
-#include <iostream>
-#include <vector>
+﻿#include <iostream>
 #include <math.h>
 #include <fstream>
 
+#define PI 3.14159265359
+
 int main()
 {
-	std::vector <double> Hc;
+	double R0 = 6378.1; //ekvatorial'niy radius Earth
+	double grav = 398600.4415;
+	double Vc, Vi;
 
-	int eps = 100;
-	for (int i{ 0 }; i <= 60000; i += eps) {
-		Hc.push_back(i);
-	}
 
-	double R0 = 6371;
-	double grav = 398600.4;
-	double Vc;
-
-	
 	std::ofstream fout;
 
 	try {
 		fout.open("Hc_Vc.txt");
-		for (auto& HIGH : Hc) {
-			Vc = sqrt((grav / (R0 + HIGH)));
-			fout << HIGH << "\t" << Vc << std::endl;
-		}
-		std::cout << "Data was succesfully written.";
-		fout.close();
-		return 0;
 	}
-	
 	catch (std::exception& ex) {
 		std::cout << "File opening error!";
 		fout.close();
 		return 0;
 	}
+
+
+	for (double Hc{ 0 }; Hc <= 60000; Hc += 100) {
+
+		Vc = sqrt((grav / (R0 + Hc)));
+		Vi = Vc / cos(PI/4);
+		std::cout << Vi << std::endl;
+		fout << Hc << "\t" << Vc;
+		fout << "\t" << Vi << std::endl;
+
+	}
+
+	std::cout << "Data was succesfully written.";
+
+
+	fout.close();
+	return 0;
 
 }
 
